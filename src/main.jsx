3c9f41a41ed0431767349ad0ead5c2c4d7da7157
srilohit <86248601+srilohit@users.noreply.github.com>
2025-09-23 17:14:46 +0530

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    }
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+        <RouterProvider router={router} />
+
+);
