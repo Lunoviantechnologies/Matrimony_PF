e3cd844329e5a3210b6ce946afb9ae60c050ed6d
bhavyasri117 <2110030117@klh.edu.in>
2025-10-27 11:07:28 +0530

    ..

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,7 +5,6 @@ import Home from "../pages/Home";
 import EditProfile from "../pages/EditProfile";
 import ProfileView from "../pages/ProfileView";
 
-
 const AppRoutes = createBrowserRouter([
     {
         path: "/",
@@ -16,20 +15,17 @@ const AppRoutes = createBrowserRouter([
                 element: <Home />
             },
             {
-<<<<<<< HEAD
                 path : "/editProfile",
                 element : <EditProfile />
             },
             {
                 path : "/viewProfile",
                 element : <ProfileView />
-            }
-            
-=======
+            },
+            {
                 path:"/register",
                 element:<Register/>
             },
->>>>>>> 5c569d7c680f3240fa882e9a7eb42bedf2e9b802
             // {
             //     path: "/login", 
             //     element: <Login />
